8cdea5c6cb92cf2172b553b3573ec780e6f8a7aa
Bobbi Henderson <bobbilhenderson@gmail.com>
2021-04-05 18:53:46 -0500

    added put routes to organize list priority

--- public/JS/todoList.js
+++ public/JS/todoList.js
@@ -1,7 +1,17 @@
 let deleteTask = document.querySelectorAll('.delete')
+let priorityUp = document.querySelectorAll('.fa-arrow-up')
+let priorityDown = document.querySelectorAll('.fa-arrow-down')
 
 Array.from(deleteTask).forEach((element)=>{
     element.addEventListener('click', removeTask)
+})
+
+Array.from(priorityUp).forEach((element)=>{
+    element.addEventListener('click', addPriority)
+})
+
+Array.from(priorityDown).forEach((element)=>{
+    element.addEventListener('click', lowerPriority)
 })
 
 async function removeTask(){
@@ -15,7 +25,46 @@ async function removeTask(){
             })
         })
         const data = await response.json()
-        console.log(data)
+        location.reload()
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+async function addPriority(){
+    let taskName = this.parentNode.parentNode.parentNode.childNodes[3].innerText
+    let priorityLevel = Number(this.parentNode.parentNode.childNodes[1].innerText)
+    console.log(taskName)
+    try {
+        const response = await fetch('increasePriority', {
+            method: 'put', 
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'taskName': taskName,
+                'priorityLevel': priorityLevel
+            })
+        })
+        const data = await response.json()
+        location.reload()
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+async function lowerPriority(){
+    let taskName = this.parentNode.parentNode.parentNode.childNodes[3].innerText
+    let priorityLevel = Number(this.parentNode.parentNode.childNodes[1].innerText)
+    console.log(priorityLevel)
+    try {
+        const response = await fetch('decreasePriority', {
+            method: 'put', 
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'taskName': taskName,
+                'priorityLevel': priorityLevel
+            })
+        })
+        const data = await response.json()
         location.reload()
     } catch(err) {
         console.log(err)
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const { response } = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 8000
@@ -24,7 +23,7 @@ MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
 
 
 app.get('/', (req, res)=>{
-    db.collection('todos').find().toArray()
+    db.collection('todos').find().sort({priority: -1}).toArray()
     .then(data=>{
         res.render('index.ejs', {info: data})
     })
@@ -32,7 +31,7 @@ app.get('/', (req, res)=>{
 })
 
 app.post('/addToDo', (req, res)=>{
-    db.collection('todos').insertOne(req.body)
+    db.collection('todos').insertOne({task: req.body.task, priority:0})
     .then(result =>{
         console.log(result)
         res.redirect('/')
@@ -45,6 +44,32 @@ app.delete('/deleteTask', (req, res)=>{
     .then(result =>{
         console.log('Task Completed')
         res.json('Task Completed')
+    })
+    .catch(error => console.error(error))
+})
+
+app.put('/increasePriority', (req, res)=>{
+    db.collection('todos').updateOne({task: req.body.taskName, priority: req.body.priorityLevel},
+        {$set:{
+            priority: req.body.priorityLevel + 1
+        }
+    })
+    .then(result =>{
+        console.log('Increased Priority')
+        res.json('Like Added')
+    })
+    .catch(error => console.error(error))
+})
+
+app.put('/decreasePriority', (req, res)=>{
+    db.collection('todos').updateOne({task: req.body.taskName, priority: req.body.priorityLevel},
+        {$set:{
+            priority: req.body.priorityLevel - 1
+        }
+    })
+    .then(result =>{
+        console.log('Decreased Priority')
+        res.json('Like Added')
     })
     .catch(error => console.error(error))
 })
